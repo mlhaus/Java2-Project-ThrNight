b2438ffbf2818a79b9a5b941b7ae47ae87d78b1e
Marc Hauschildt <marc.hauschildt@gmail.com>
2022-08-25 20:23:49 -0500

    Started Intro assignment

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java1review.Book;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My name is Marc");
+        Book book1 = new Book();
+        Book book2 = new Book("Java A Beginner's Guide", "Herbert Schildt", 850, true);
+//        System.out.println(book1);
+//        System.out.println(book2);
+        Book[] library = new Book[5];
+        library[0] = book1;
+        library[1] = book2;
+        library[2] = new Book();
+        library[3] = new Book();
+        for(Book book: library) {
+            System.out.println(book);
+        }
+        for(int i = 0; i < Book.getBookCount(); i++) {
+            System.out.println(library[i]);
+        }
     }
 }
---
+++ src/java1review/Book.java
@@ -0,0 +1,83 @@
+package java1review;
+
+public class Book {
+    private String title;
+    private String author;
+    private int numPages;
+    private boolean read;
+    public static final String DEFAULT_TITLE = "Undefined";
+    private static int bookCount = 0;
+
+    public Book() {
+        title = DEFAULT_TITLE;
+        author = DEFAULT_TITLE;
+        numPages = 1;
+        read = false;
+        bookCount++;
+    }
+
+    public Book(String title, String author, int numPages, boolean read) {
+        this.title = title;
+        this.author = author;
+        this.numPages = numPages;
+        this.read = read;
+        bookCount++;
+    }
+
+    public static int getBookCount() {
+        return bookCount;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if (title.equals("")) {
+            throw new IllegalArgumentException("The title is required.");
+        } else {
+            this.title = title;
+        }
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        if (author.equals("")) {
+            throw new IllegalArgumentException("The author is required.");
+        } else {
+            this.author = author;
+        }
+    }
+
+    public int getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(int numPages) {
+        if (numPages < 1) {
+            throw new IllegalArgumentException("There must be at least one page.");
+        }
+        this.numPages = numPages;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", numPages=" + numPages +
+                ", read=" + read +
+                '}';
+    }
+}
